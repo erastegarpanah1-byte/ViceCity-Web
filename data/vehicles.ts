8b8a8a9592c333b005eae5650928b6d05774b24b
Eilya Rastegarpanah <e.rastegarpanah1@gmail.com>
2026-09-04 00:02:32 +0330

    feat: add vehicle catalogue

---
+++ data/vehicles.ts
@@ -0,0 +1,7 @@
+export const vehicles=[
+ {id:'sunset-sedan',name:'Sunset Sedan',kind:'sedan',maxSpeed:42,health:100,fuel:100},
+ {id:'coast-sport',name:'Coast Sport',kind:'sports',maxSpeed:58,health:85,fuel:100},
+ {id:'metro-suv',name:'Metro SUV',kind:'suv',maxSpeed:38,health:140,fuel:100},
+ {id:'marina-runner',name:'Marina Runner',kind:'boat',maxSpeed:28,health:90,fuel:100},
+ {id:'skyhawk',name:'Skyhawk',kind:'helicopter',maxSpeed:34,health:110,fuel:100}
+] as const;
